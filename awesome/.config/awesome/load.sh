958303d8e9314e925cfbcea0468c488084e7f089
wilssonmartee <wilssonm@gmail.com>
2020-02-07 00:00:30 -0400

    fixed load.sh

--- awesome/.config/awesome/load.sh
+++ awesome/.config/awesome/load.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 #
 
+# Power Manager
 xfce4-power-manager &
-nm-applet --no-agent &
+# udiskie (automount usb)
 udiskie &
+# Enable tap (touchpad)
 synclient TapButton1=1 &
 
 # Start polkit (para autentificaion gparted y otros)
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+
+# Network manager applet (tray icon)
+nm-applet --no-agent &
