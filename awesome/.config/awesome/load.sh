594d4c8637d80b963003ec8572f87287a52e5634
wilssonmartee <wilssonm@gmail.com>
2020-02-07 00:24:56 -0400

    fixed load.sh

--- awesome/.config/awesome/load.sh
+++ awesome/.config/awesome/load.sh
@@ -11,5 +11,6 @@ synclient TapButton1=1 &
 # Start polkit (para autentificaion gparted y otros)
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 
+sleep 8
 # Network manager applet (tray icon)
 nm-applet --no-agent &
