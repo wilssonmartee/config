812149e12100796d2bf793d7b4ecb4395ba831e5
wilssonmartee <wilssonm@gmail.com>
2020-02-11 15:45:19 -0400

    fixed reflector script

--- awesome/.config/reflector/reflector.sh
+++ awesome/.config/reflector/reflector.sh
@@ -3,12 +3,12 @@
 #
 
 date=$(date +%D)
-lastsync="$(cat ~/.config/reflector/.lastsync.txt)"
+lastsync="$(cat .config/reflector/.lastsync.txt)"
 
 if [ "$date" != "$lastsync" ] ; then
 echo ":: Updating mirrorlist..."
-> ~/.config/reflector/.lastsync.txt
-echo $date >> ~/.config/reflector/.lastsync.txt
+> .config/reflector/.lastsync.txt
+echo $date >> .config/reflector/.lastsync.txt
 sudo reflector --country 'United States' -l 5 --sort rate --save /etc/pacman.d/mirrorlist
 fi
 exit
